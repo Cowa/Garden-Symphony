06c47868d9b7efef7ba1376054b9ef54ca764bdd
Brice Thomas <brice.p.thomas@gmail.com>
2018-04-21 19:33:54 +0200

    Implement grass growing

--- scripts/items/seeds/PlantedSeed.gd
+++ scripts/items/seeds/PlantedSeed.gd
@@ -1,15 +1,20 @@
 extends Node2D
 
+const INITIAL_GROWING_STATE = -1
+
 var state = {
-	"growing": 0
+	"tick": 0,
+	"growing": INITIAL_GROWING_STATE
 }
 
 func _ready():
 	$Tick.connect("timeout", self, "_on_tick")
+	# Directly play planted animation
+	$AnimationPlanted.play("planted")
 
 func _on_tick():
 	# Grow seed on tick
-	state.growing += 1
+	state.tick += 1
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
---
+++ scripts/items/seeds/grass/PlantedGrass.gd
@@ -0,0 +1,30 @@
+extends "res://scripts/items/seeds/PlantedSeed.gd"
+
+const GROWING_STATE = {
+	"zero": 0,
+	"one": 1,
+	"two": 2
+}
+
+func _ready():
+	pass
+
+func _on_tick():
+	._on_tick()
+	print("tick...")
+	if state.tick == 2:
+		$GrowingState/Animation.play("grow_to_0")
+		state.growing = GROWING_STATE.zero
+	elif state.tick == 5:
+		$GrowingState/Animation.play("grow_to_1")
+		state.growing = GROWING_STATE.one
+	elif state.tick == 10:
+		$GrowingState/Animation.play("grow_to_2")
+		state.growing = GROWING_STATE.two
+		# Last growing state
+		$Tick.queue_free()
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
